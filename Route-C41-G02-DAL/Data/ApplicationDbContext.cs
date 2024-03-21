157fa520a1d7490b4d96b9ba717055e25b56a488
Mohamed <moham@MOHAMEDZZ>
2024-03-21 07:07:19 +0200

    DAL for Department Module

---
+++ Route-C41-G02-DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Route_C41_G02_DAL.Data.Configrations;
+using Route_C41_G02_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route_C41_G02_DAL.Data
+{
+    internal class ApplicationDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Server = .; Database = MVC03; Trusted_Connection = True");
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration(new DepartmentConfigrations());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ Route-C41-G02-DAL/Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route_C41_G02_DAL.Models
+{
+    internal class Department
+    {
+        public int Id { get; set; }  // PK For developer
+
+        
+        public string Name { get; set; } // Code for User
+        
+       
+        public string Code { get; set; }
+        public DateTime DateOfCreation { get; set; }
+    }
+}
