11590db9648d68945b25917120ee8bc2de116df8
Mohamed <moham@MOHAMEDZZ>
2024-03-21 09:43:06 +0200

    Business Logic Layer

---
+++ Route-C41-G02-BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,22 @@
+﻿using Route_C41_G02_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route_C41_G02_BLL.Interfaces
+{
+    internal interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll();
+        Department Get(int id);
+
+        int Add(Department department);
+        int Update(Department department);
+        int Delete(Department department);
+
+
+
+    }
+}
