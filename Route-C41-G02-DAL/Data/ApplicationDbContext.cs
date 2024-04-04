f8b6cd8f0700278dbb8ea04c2d30e52babef5243
Mohamed <moham@MOHAMEDZZ>
2024-04-04 05:54:39 +0200

    Microsoft Identity Package

--- Route-C41-G02-DAL/Data/ApplicationDbContext.cs
+++ Route-C41-G02-DAL/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Route_C41_G02_DAL.Data.Configrations;
 using Route_C41_G02_DAL.Models;
 using System;
@@ -10,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace Route_C41_G02_DAL.Data
 {
-    public class ApplicationDbContext :DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
@@ -18,17 +20,15 @@ namespace Route_C41_G02_DAL.Data
             
         }
 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //    => optionsBuilder.UseSqlServer("Server = .; Database = MVC03; Trusted_Connection = True");
-
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //modelBuilder.ApplyConfiguration(new DepartmentConfigrations());
+            base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
 
         public DbSet<Department> Departments { get; set; }
         public DbSet<Employee> Employees { get; set; }
+
+
     }
 }
